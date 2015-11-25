f3c64d3e628fab17f36923bde2884621d9b30103
kenystev <kjestevez.0795@gmail.com>
2015-11-25 11:27:39 -0600

    I dont know what im doing

--- main.cpp
+++ main.cpp
@@ -5,8 +5,7 @@ using namespace std;
 
 //LIBRERIAS DE ALLEGRO
 #include <allegro5/allegro.h>
-#include "allegro5/allegro_image.h"
-#include "allegro5/allegro_native_dialog.h"
+#include <allegro5/allegro_image.h>
 #include <allegro5/allegro_audio.h>
 #include <allegro5/allegro_acodec.h>
 #include <allegro5/allegro_font.h>
