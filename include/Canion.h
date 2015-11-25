7090dbcb80ba3035ba4e7ad9bf5ccc2a4ff4982a
rnexer <rnexer2995@gmail.com>
2015-11-25 12:29:24 -0600

    Global classes

---
+++ include/Canion.h
@@ -0,0 +1,15 @@
+#ifndef CANION_H
+#define CANION_H
+
+
+class Canion
+{
+    public:
+        Canion();
+        virtual ~Canion();
+
+    protected:
+    private:
+};
+
+#endif // CANION_H
---
+++ include/Entidad.h
@@ -0,0 +1,37 @@
+#ifndef ENTIDAD_H
+#define ENTIDAD_H
+
+
+//Librerías y archivos necesarios
+#include "Box.h"
+#include <allegro5/allegro.h>
+#include "allegro5/allegro_image.h"
+#include "allegro5/allegro_native_dialog.h"
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
+#include <iostream>
+#include <map>
+#include <vector>
+#include <list>
+#include <fstream>
+
+using namespace std;
+
+class Entidad
+{
+    public:
+        //Atributos
+        bool colisionado;
+        Box *detalles = new Box(0,0,0,0); //donde estará y de donde se comprueban las colisiones
+        float velocity = 1; //cuanto se mueve
+        string tipoObjeto = ""; //muestra que objeto es: obstacle, disparo, enemy, etc.
+        virtual void act(ALLEGRO_EVENT* ev) = 0;
+        virtual void draw() = 0;//dibujo de la figura
+        bool colision(Box*detalles, Box* pCaja);//detectar colisiones con otros objetos
+        Entidad();
+        virtual ~Entidad();
+    protected:
+    private:
+};
+
+#endif // ENTIDAD_H
